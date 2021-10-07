f4878bce68b17ddd39d315b0b8e01c767ca4ec33
CFGS <CFGS@DESKTOP-H1K1N8D>
2021-10-07 16:21:17 +0200

    Unidad1

--- src/demos/CrearFicheros.java
+++ src/demos/CrearFicheros.java
@@ -34,11 +34,20 @@ public class CrearFicheros {
         }
         //Crear directorios
         String directorio1="C:\\prueba2";
-        String directorio2="/carpeta1/carpeta2/carpeta3";
+        String directorio2="carpeta1/carpeta2/carpeta3";
         String directorio3="C:\\prueba3\\C1\\C2\\C3";
         //Crear directorio prueba2
         if (new File(directorio1).mkdir()) {
             System.out.println("Directorio "+directorio1+" creado");
+        }
+        //Segunda opcion
+        File fichero2=new File(directorio1);
+        if (fichero2.mkdir()) {
+            System.out.println("Directorio "+directorio1+" creado");
+        }
+        //Crear directorios (directorio2)
+        if (new File(directorio2).mkdirs()) {
+            System.out.println("Directorio "+directorio2+" creado");
         }
     } //Fin del main
 
