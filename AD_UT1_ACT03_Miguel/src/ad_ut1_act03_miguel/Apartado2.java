11fb55fb383806b476f97498e4bff2067dc4d793
memeguel <91274432+memeguel@users.noreply.github.com>
2021-10-18 18:25:09 +0200

    Add files via upload

--- AD_UT1_ACT03_Miguel/src/ad_ut1_act03_miguel/Apartado2.java
+++ AD_UT1_ACT03_Miguel/src/ad_ut1_act03_miguel/Apartado2.java
@@ -2,40 +2,40 @@ package ad_ut1_act03_miguel;
 
 import java.io.File;
 
-
 /**
  *
  * @author CFGS
  */
-
 public class Apartado2 {
+
     public static void main(String[] args) {
-    	String ruta="/users/"+System.getProperty("user.name")+"/documents";
-        File carpeta=new File(ruta);
-        String t="";
-        verContenido(carpeta,t);
+        String ruta = "/users/" + System.getProperty("user.name") + "/documents";
+        File carpeta = new File(ruta);
+        String t = "";
+        verContenido(carpeta, t);
     } //Fin del main
-    public static void verContenido(File carpeta,String t) {
-    	System.out.printf("%s%s --> %s%n",t,"Directorio",carpeta.getName());
-    	int i=0;
-        File[] ficheros=carpeta.listFiles();
-        if (ficheros!=null) {
-        	for (File fichero : ficheros) {
+
+    public static void verContenido(File carpeta, String t) {
+        System.out.printf("%s%s --> %s%n", t, "Directorio", carpeta.getName());
+        int i = 0;
+        File[] ficheros = carpeta.listFiles();
+        if (ficheros != null) {
+            for (File fichero : ficheros) {
                 if (fichero.isDirectory()) {
-                	t+="    ";
-                    verContenido(fichero,t);
-                    i=t.length();
-                    t=t.substring(0, i-4);
-                }else{
-                	t+="    ";
-                    System.out.printf("%s%s --> %s%n",t,"Archivo",fichero.getName());
-                    i=t.length();
-                    t=t.substring(0, i-4);
+                    t += "    ";
+                    verContenido(fichero, t);
+                    i = t.length();
+                    t = t.substring(0, i - 4);
+                } else {
+                    t += "    ";
+                    System.out.printf("%s%s --> %s%n", t, "Archivo", fichero.getName());
+                    i = t.length();
+                    t = t.substring(0, i - 4);
                 }
-		
+
             }
-		}
-            
+        }
+
     }
-    
+
 } //Fin de la clase Apartado2
