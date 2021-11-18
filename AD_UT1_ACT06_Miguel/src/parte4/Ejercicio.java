a56e50ee4fe2846e24823521da2092c11c647111
memeguel <91274432+memeguel@users.noreply.github.com>
2021-11-18 20:54:16 +0100

    Add files via upload

--- AD_UT1_ACT06_Miguel/src/parte4/Ejercicio.java
+++ AD_UT1_ACT06_Miguel/src/parte4/Ejercicio.java
@@ -1,6 +1,12 @@
 package parte4;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 
 /**
@@ -9,10 +15,23 @@ import java.io.File;
  */
 
 public class Ejercicio {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
         File f=new File("src/parte4/fichero.dat");
-        
-        
+        BufferedWriter bw=new BufferedWriter(new FileWriter(f));
+        System.out.println("Introduzca una frase:");
+        bw.write(sc.nextLine());
+        bw.flush();
+        InputStreamReader isr=new InputStreamReader(new FileInputStream(f));
+        int c;
+        while ((c=isr.read())!=-1) {            
+            if ((char)c!=' ') {
+                System.out.print((char)c);
+            }
+        }
+        isr.close();
+        bw.close();
+        sc.close();
     } //Fin del main
 
 } //Fin de la clase Ejercicio
